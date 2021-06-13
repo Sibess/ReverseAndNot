8c6f7d50b7bf8191bfc96a9384ecafe30f38c2e3
Siarhei <taler34@gmail.com>
2021-06-13 23:23:08 +0300

    ReverseAndNot

---
+++ ReverseAndNot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseAndNot
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseAndNot(123456789));
+        }
+
+        public static string ReverseAndNot(int i)
+
+        {
+            string myString = i.ToString();
+
+            string reverseValue = new string(myString.Reverse().ToArray());
+
+            string result = String.Concat(reverseValue, myString);
+
+            return result;
+        }
+    }
+}
